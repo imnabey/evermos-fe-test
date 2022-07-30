4b20de9b31daf91b5826e1d58842ab76f54b9adf
imnabey <nabilahpermata6@gmail.com>
2022-07-30 16:37:30 +0700

    add cors-anywhere

--- utils/requests.ts
+++ utils/requests.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://makeup-api.herokuapp.com';
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/http://makeup-api.herokuapp.com';
 
 const requests = {
   fetchCampaign: `${BASE_URL}/api/v1/products.json?brand=maybelline`,
